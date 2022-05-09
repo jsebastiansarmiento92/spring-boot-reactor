63a0c7981ac0c45bffb686a9e3ded271e9b2777c
jusarmie <jusarmie@bancolombia.com.co>
2022-05-09 16:50:35 -0500

    Operador FlatMap

--- src/main/java/com/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/reactor/app/SpringBootReactorApplication.java
@@ -1,6 +1,7 @@
 package com.reactor.app;
 
-import javax.management.RuntimeErrorException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.reactor.app.models.Usuario;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
@@ -23,15 +25,20 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("juna calme" , "juan sarmiento" , "pedro anito","andres cortes", "Bruce Lee", "Bruce Willis", "Juan Mengano");
-				nombres.map(nombre -> nombre.toLowerCase()).doOnNext(nombre->{
-					if(nombre.isEmpty()) {
-						throw new RuntimeException("Nobres no pueden ir vacios");
-					}
-					System.out.print("nombre: "+ nombre);
-				});
+		exampleIterable();
 		
-				Flux<Usuario> usuarios = nombres.map(nombre ->new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase())).
+	}
+	public void exampleFlatMap() throws Exception {
+		List<String> usuariosList = new ArrayList<>();
+		usuariosList.add("juna calme" );
+		usuariosList.add("juan sarmiento");
+		usuariosList.add( "pedro anito");
+		usuariosList.add("andres cortes");
+		usuariosList.add("Bruce Lee");
+		usuariosList.add("Bruce Willis");
+		usuariosList.add("Juan Mengano");
+		
+		Flux.fromIterable(usuariosList).map(nombre ->new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase())).
 				filter(usuario -> usuario.getName().equalsIgnoreCase("bruce")).
 				doOnNext(usuario ->{
 					if (usuario == null) {
@@ -43,24 +50,43 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 					usuario.setName(nombre);
 					return usuario;
 					
-				});
-		nombres.subscribe(e -> Log.info(e),error ->Log.error("nombres vacios"),new Runnable() {
+				}).subscribe(e -> Log.info(e.getName()),error ->Log.error("nombres vacios"),new Runnable() {
 			
-			@Override
+			@Override	
 			public void run() {
-				// TODO Auto-generated method stub
 				System.err.println("cambios e impresiones reaziadas");
 			}
 		});
-		usuarios.subscribe(e -> Log.info(e.toString()),
-				error -> Log.error(error.getMessage()),
-				new Runnable() {
-					@Override
-					public void run() {
-						Log.info("ha ejecutado correctamente la ejecucion del observable");
-						
-					}
-				});
 	}
-
+	public void exampleIterable() throws Exception {
+		List<String> usuariosList = new ArrayList<>();
+		usuariosList.add("juna calme" );
+		usuariosList.add("juan sarmiento");
+		usuariosList.add( "pedro anito");
+		usuariosList.add("andres cortes");
+		usuariosList.add("Bruce Lee");
+		usuariosList.add("Bruce Willis");
+		usuariosList.add("Juan Mengano");
+		
+		Flux<String> nombres = Flux.fromIterable(usuariosList);/*Flux.just("juna calme" , "juan sarmiento" , "pedro anito","andres cortes", "Bruce Lee", "Bruce Willis", "Juan Mengano");*/
+		
+				nombres.map(nombre ->new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase())).
+				flatMap(usuario -> {
+					if (usuario.getName().equalsIgnoreCase("bruce")) {
+						return Mono.just(usuario);
+					}else  return Mono.empty();
+				}).
+				doOnNext(usuario ->{
+					if (usuario == null) {
+						throw new RuntimeException("Nombres no pueden ser vacios");
+					}
+					System.out.println(usuario.getName() + " " + usuario.getLasName());
+				}).map(usuario -> {
+					String nombre = usuario.getName().toLowerCase();
+					usuario.setName(nombre);
+					return usuario;
+				}).subscribe(e -> Log.info(e.toString()));
+		
+	}
+	
 }
