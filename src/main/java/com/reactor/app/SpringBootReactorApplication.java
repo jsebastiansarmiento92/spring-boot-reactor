c278a5b1aefbdabf68efd607c1addce32ead2632
jusarmie <jusarmie@bancolombia.com.co>
2022-05-09 15:38:39 -0500

    observables inmutables

--- src/main/java/com/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.reactor.app;
 
+import javax.management.RuntimeErrorException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -21,8 +23,15 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<Usuario> nombres = Flux.just("juna calme" , "juan sarmiento" , "pedro anito","andres cortes", "Bruce Lee", "Bruce Willis", "Juan Mengano").
-				map(nombre ->new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase())).
+		Flux<String> nombres = Flux.just("juna calme" , "juan sarmiento" , "pedro anito","andres cortes", "Bruce Lee", "Bruce Willis", "Juan Mengano");
+				nombres.map(nombre -> nombre.toLowerCase()).doOnNext(nombre->{
+					if(nombre.isEmpty()) {
+						throw new RuntimeException("Nobres no pueden ir vacios");
+					}
+					System.out.print("nombre: "+ nombre);
+				});
+		
+				Flux<Usuario> usuarios = nombres.map(nombre ->new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase())).
 				filter(usuario -> usuario.getName().equalsIgnoreCase("bruce")).
 				doOnNext(usuario ->{
 					if (usuario == null) {
@@ -35,10 +44,17 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 					return usuario;
 					
 				});
-		nombres.subscribe(e -> Log.info(e.toString()),
+		nombres.subscribe(e -> Log.info(e),error ->Log.error("nombres vacios"),new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				System.err.println("cambios e impresiones reaziadas");
+			}
+		});
+		usuarios.subscribe(e -> Log.info(e.toString()),
 				error -> Log.error(error.getMessage()),
 				new Runnable() {
-					
 					@Override
 					public void run() {
 						Log.info("ha ejecutado correctamente la ejecucion del observable");
