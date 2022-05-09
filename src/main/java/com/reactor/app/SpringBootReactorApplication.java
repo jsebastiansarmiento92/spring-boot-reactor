b4ea1b39c220461d97602fdd733b7bb2b0150f2f
jusarmie <jusarmie@bancolombia.com.co>
2022-05-09 14:15:07 -0500

    first commit

---
+++ src/main/java/com/reactor/app/SpringBootReactorApplication.java
@@ -0,0 +1,33 @@
+package com.reactor.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class SpringBootReactorApplication implements CommandLineRunner {
+
+	private static final Logger Log =  LoggerFactory.getLogger(SpringBootApplication.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> nombres = Flux.just("juna" , "juan" , "pedro","").
+				doOnNext(e ->{
+					if (e.isEmpty()) {
+						throw new RuntimeException("Nombres no pueden ser vacios");
+					}
+					System.out.println(e);
+				});
+		nombres.subscribe(e -> Log.info(e),
+				error -> Log.error(error.getMessage()));
+	}
+
+}
